b9a30e10b8f301c6ad5b37acb3f0a738134879e8
Evgeniy Leontyev <evgeniy_leontyev@proton.me>
2025-02-09 19:32:41 +0300

    Examples update

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -7,7 +7,7 @@ async fn main() {
     let mut audio_player = morse_player::AudioPlayer::new();
     audio_player.set_speed(100.0);
     audio_player.set_text(&vec!['H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D'].to_vec());
-    audio_player.set_text_type(morse_player::TextType::Digits);
+    audio_player.set_text_type(morse_player::TextType::Letters);
     audio_player.set_text_additions(morse_player::TextAdditions::None);
     audio_player.play().await;
     tokio::time::sleep(Duration::from_millis(500)).await;
